b55a347258b0fc3eda47f4ccd3e2c0db595b4957
cyjseagull <1547158861@qq.com>
2021-10-13 21:22:28 +0800

    modify bcos-dispatcher to bcos-scheduler && add bcos-group-manager (#42)

--- cmake/projects/bcos-group-manager/hunter.cmake
+++ cmake/projects/bcos-group-manager/hunter.cmake
@@ -11,22 +11,22 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    bcos-dispatcher
+    bcos-group-manager
     VERSION
-    3.0.0-2903b298
+    3.0.0-d3ef2a00
     URL
-    "https://github.com/FISCO-BCOS/bcos-dispatcher/archive/2903b298ec15ac4e96d4934ee4bf3cccbd489082.tar.gz"
+    "https://github.com/FISCO-BCOS/bcos-group-manager/archive/d3ef2a002301003b781819d49f197594f607f331.tar.gz"
     SHA1
-    654ac03f71eaa15a26f33c41734f25f908f49140
+    44733aee8968466f042c313f6c726906f3f05068
 )
 
 hunter_cmake_args(
-    bcos-dispatcher
+    bcos-group-manager
     CMAKE_ARGS
     CMAKE_INSTALL_LIBDIR=lib
     CMAKE_INSTALL_BINDIR=bin
     CMAKE_INSTALL_INCLUDEDIR=include
 )
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(bcos-dispatcher)
-hunter_download(PACKAGE_NAME bcos-dispatcher)
+hunter_cacheable(bcos-group-manager)
+hunter_download(PACKAGE_NAME bcos-group-manager)
